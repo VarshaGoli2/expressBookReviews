2bf27a30f99593bf666949dfaa662358b689d0a1
VarshaGoli2 <varshagoli2@gmail.com>
2024-11-05 00:49:19 -0500

    task4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,7 +47,15 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const booksByTitle = Object.values(books).filter(book => book.title === title);
+  
+  if (booksByTitle.length > 0) {
+    res.status(200).json(booksByTitle);
+  } else {
+    res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
 //  Get book review
